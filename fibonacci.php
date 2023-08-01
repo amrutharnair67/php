151d48cb82f9eefbd5ca9ada11be6fbd71e1ec57
Amrutha R Nair <rnairamrutha67@gmail.com>
2023-08-02 00:45:44 +0530

    2nd commit

---
+++ fibonacci.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html >
+
+<body>
+  <?php
+    function fibonacci($n) {
+        $fib = array(0, 1);
+        for ($i = 2; $i < $n; $i++) {
+            $fib[$i] = $fib[$i - 1] + $fib[$i - 2];
+        }
+        return $fib;
+    }
+    
+    // Change the value of $length to generate a Fibonacci sequence of different lengths
+    $length = 10;
+    $result = fibonacci($length);
+    
+    // Output the Fibonacci sequence
+    echo "Fibonacci sequence of length $length: ";
+    foreach ($result as $num) {
+        echo $num . " ";
+    }
+   
+  ?>
+</body>
+</html>
